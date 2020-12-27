1683869126aa185a2242a5af99306fceb7a2e36a
Yomna-Sabah <55870140+Yomna-Sabah@users.noreply.github.com>
2020-12-26 22:54:11 -0800

    Update Time.c

--- Time.c
+++ Time.c
@@ -7,6 +7,7 @@
 
 static unsigned int hours = 0;
 static unsigned int mins = 0;
+static tTIM_Mode current_Mode = NORMAL;
 
 void TIM_Init(unsigned int Counter)
 {
@@ -18,16 +19,87 @@ void TIM_Init(unsigned int Counter)
 
 void TIM_Update(void)
 {
-    if(TIM_GetMin()<59){
-    mins = TIM_GetMin()+1;
-    }
-    else if(TIM_GetHour()<23) {
-        mins  = 0;
-        hours = TIM_GetHour()+1;
-    }
-    else{
-        mins  = 0;
-        hours = 0;
+    switch (current_Mode)
+    {
+        case NORMAL:
+            if (PB_GetState(PB_SET) == PB_PRE_RELEASED)
+            {
+                current_Mode = SET_HOURS;
+            }
+            if(TIM_GetMin()<59)
+            {
+                mins = TIM_GetMin()+1;
+            }
+            else if(TIM_GetHour()<23)
+            {
+                mins  = 0;
+                hours = TIM_GetHour()+1;
+            }
+            else
+            {
+                mins  = 0;
+                hours = 0;
+            }
+        break;
+        case SET_HOURS:
+            if (PB_GetState(PB_SET) == PB_PRE_RELEASED)
+            {
+                current_Mode = SET_MINUTES;
+            }
+            if (PB_GetState(PB_PLUS) == PB_PRE_PRESSED)
+            {
+                if(TIM_GetHour() < 23)
+                {
+                    hours = TIM_GetHour()+1;
+                }
+                else
+                {
+                    hours = 0;
+                }
+            }
+            if (PB_GetState(PB_MINUS) == PB_PRE_PRESSED)
+            {
+                if(TIM_GetHour() > 0)
+                {
+                    hours = TIM_GetHour()-1;
+                }
+                else
+                {
+                    hours = 23;
+                }
+            }
+        break;
+        case SET_MINUTES:
+            if (PB_GetState(PB_SET) == PB_PRE_RELEASED)
+            {
+                current_Mode = NORMAL;
+            }
+            if (PB_GetState(PB_PLUS) == PB_PRE_PRESSED)
+            {
+                if(TIM_GetHour() < 59)
+                {
+                    mins = TIM_GetMin()+1;
+                }
+                else
+                {
+                    mins = 0;
+                }
+            }
+            if (PB_GetState(PB_MINUS) == PB_PRE_PRESSED)
+            {
+                if(TIM_GetHour() > 0)
+                {
+                    mins = TIM_GetMin()-1;
+                }
+                else
+                {
+                    mins = 59;
+                }
+            }
+        break;
+        default:
+            /* Should not be here */
+        break;
     }
 }
 
